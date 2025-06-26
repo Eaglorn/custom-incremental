2cbcfb127127eee510d701eca2707c42d746cadf
Eaglorn <lexus1.45@mail.ru>
2025-06-26 16:49:09 +1000

    Add big number libraries and update IndexPage UI
    Added bignumber.js, break_eternity.js, and break_infinity.js dependencies and boot files for each. Updated decimal.js boot file to set precision and rounding. Refactored IndexPage.vue to use break_eternity.js for number handling, added tabbed UI, and implemented timer-based number increment logic.

---
+++ src/boot/breakinfinity.ts
@@ -0,0 +1,3 @@
+import Decimal from 'break_infinity.js';
+
+export { Decimal };
--- src/boot/decimal.ts
+++ src/boot/decimal.ts
@@ -1,3 +1,8 @@
-import { Decimal } from 'decimal.js';
+import Decimal from 'decimal.js';
+
+Decimal.set({
+  precision: 4,
+  rounding: Decimal.ROUND_HALF_UP,
+});
 
 export { Decimal };
